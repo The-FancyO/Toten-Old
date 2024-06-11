8b1582dcf9e2a6cfc556eac6d2364e2ef784ce33
The-FancyO <orabaya@outlook.com>
2024-06-12 01:10:10 +0300

    sync

--- include/player.hpp
+++ include/player.hpp
@@ -34,14 +34,14 @@ private:
         {"test", {LoadTexture("res/entity/player/test.png"), 1}}
     };
 
-    // Camera
-    Camera2D cam;
-
     // Vectors
     Vector2 pos;
     Vector2 vel;
     Vector2 scale;
     Vector2 size;
+
+    // Camera
+    Camera2D cam;
 
     // Rects
     Rectangle src;
